6774b3472bc5769dbcdc161d1843d851b3704798
Reegan <reegan.gunasekaran@neosoftmail.com>
2024-07-05 12:40:01 +0530

    updating

--- src/Components/ReactFolder/AboutHooks/AboutHooks.js
+++ src/Components/ReactFolder/AboutHooks/AboutHooks.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import {
   ABOUT_USE_CALLBACK,
   ABOUT_USE_CONTEXT,
@@ -13,49 +13,52 @@ import {
   ABOUT_USE_REF,
   ABOUT_USE_STATE,
 } from "../../Routers/RouteEndpoints";
+import "./HookStyle.css";
 
 const AboutHooks = () => {
   return (
-    <div style={{ padding: "20px" }}>
+    <div >
       <h1>What are React Hooks?</h1>
       <p>
         React Hooks are functions that let you use state and other React
         features in functional components. They allow you to use state and other
         React features without writing a class.
       </p>
-      <NavLink to={ABOUT_USE_STATE} style={{ padding: "20px" }}>
-        useState Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_EFFECT} style={{ padding: "20px" }}>
-        useEffect Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_REDUCER} style={{ padding: "20px" }}>
-        useReducer Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_CONTEXT} style={{ padding: "20px" }}>
-        useContext Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_CALLBACK} style={{ padding: "20px" }}>
-        useCallback Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_CONTEXT_DISPLAY} style={{ padding: "20px" }}>
-        use context display
-      </NavLink>
-      <NavLink to={ABOUT_USE_REF} style={{ padding: "20px" }}>
-        use ref Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_MEMO} style={{ padding: "20px" }}>
-        use memo Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_LAYOUT_EFFECT} style={{ padding: "20px" }}>
-        use Layout Effect
-      </NavLink>
-      <NavLink to={ABOUT_USE_IMPERATIVE_HANDLE} style={{ padding: "20px" }}>
-        use memo Hook
-      </NavLink>
-      <NavLink to={ABOUT_USE_DEBUG_VALUE} style={{ padding: "20px" }}>
-        use memo Hook
-      </NavLink>
+      <div>
+        <NavLink to={ABOUT_USE_STATE} className="contains">
+          useState Hook
+        </NavLink>
+        <NavLink to={ABOUT_USE_EFFECT} className="contains">
+          useEffect Hook
+        </NavLink>
+        <NavLink to={ABOUT_USE_REDUCER} className="contains">
+          useReducer Hook
+        </NavLink>
+        <Link to={ABOUT_USE_CONTEXT} className="contains">
+          <span>useContext Hook</span>
+        </Link>
+        <NavLink to={ABOUT_USE_CALLBACK} className="contains">
+          useCallback Hook
+        </NavLink>
+        <NavLink to={ABOUT_USE_CONTEXT_DISPLAY} className="contains">
+          use context display
+        </NavLink>
+        <NavLink to={ABOUT_USE_REF} className="contains">
+          use ref Hook
+        </NavLink>
+        <NavLink to={ABOUT_USE_MEMO} className="contains">
+          use memo Hook
+        </NavLink>
+        <NavLink to={ABOUT_USE_LAYOUT_EFFECT} className="contains">
+          use Layout Effect
+        </NavLink>
+        <NavLink to={ABOUT_USE_IMPERATIVE_HANDLE} className="contains">
+          use imperative handle
+        </NavLink>
+        <NavLink to={ABOUT_USE_DEBUG_VALUE} className="contains">
+          use debug value
+        </NavLink>
+      </div>
     </div>
   );
 };
